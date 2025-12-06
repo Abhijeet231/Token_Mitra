05543cdd8156058b0232d685a4d7eed9165cbbe3
Ramu kaka <pikupc01@gmail.com>
2025-12-06 13:06:08 +0530

    feat: set up backend with Express, MongoDB connection, and constants

---
+++ backend/src/index.js
@@ -0,0 +1,15 @@
+import app from "./app.js";
+import connectDB from "./db/index.js";
+
+connectDB()
+.then(
+ () => {
+    app.listen(process.env.PORT, () => {
+        console.log(`Server is running on PORT: ${process.env.PORT}`)
+    });
+ }
+)
+.catch((err) => {
+    console.log('MONGODB Connection Failed!', err)
+    process.exit(1);
+});
