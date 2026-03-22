29056fd6a27ab551958f6c1c0cae1d48b5b13062
Ramu kaka <pikupc01@gmail.com>
2026-03-22 12:19:35 +0530

    refactor: add copy-redirects plugin to vite.config.js for handling redirects

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,18 +2,26 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite"
 import path from "path"
+import fs from "fs"
 
-
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     tailwindcss(),
-    react()
+    react(),
+    {
+      name: 'copy-redirects',
+      closeBundle() {
+        fs.copyFileSync(
+          path.resolve(__dirname, 'public/_redirects'),
+          path.resolve(__dirname, 'dist/_redirects')
+        )
+        console.log('✅ _redirects copied to dist/')
+      }
+    }
   ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  publicDir: "public",
 })
